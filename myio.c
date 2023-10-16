f0b03b742eeeac9fcc9f23707271d21579e2732e
Kumar <kusumak@basin.cs.middlebury.edu>
2023-10-15 21:23:30 -0400

    Initial commit for myopen, myread, myseek

--- myio.c
+++ myio.c
@@ -13,40 +13,45 @@
 #include <limits.h>
 #include "myio.h"
 
-// Define a buffer size
 #define bufferSize 8
 
 /*
  * myopen
  */
 
-// call system call open, declare buffer , check errors
+// call system call open, malloc space for buffer , check errors
 MYFILE *myopen(const char* pathname, int flags) {
     MYFILE *file;
-    int fd;
-    if ((file = malloc(sizeof(MYFILE))) == NULL) {
+    if ((file = (MYFILE *)malloc(sizeof(MYFILE))) == NULL) {
         perror("malloc");
         return NULL;
     }
-    // Open the file using the passed flag and assume a mode of 0666
-    if ((fd = open(pathname, flags, 0666)) == -1) {
-        perror("open");
-        free(file);
+
+    int validFlags = O_CREAT | O_WRONLY | O_RDONLY | O_RDWR | O_TRUNC;
+    if ((flags & validFlags) != flags) {
+        printf("%s\n", "Not a valid flag");
         return NULL;
     }
-    // Allocate heap memory to buffer
+
+    //assume a mode of 0666
+    if ((file->fd = open(pathname, flags, 0666)) == -1) {
+        perror("open");
+        return NULL;
+    }
+
     if ((file->buf = malloc(bufferSize)) == NULL) {
         perror("malloc");
+        return NULL;
     }
+
     // Initialize other fields included in struct
-    file->fd = fd;
     file->flags = flags;
     file->bufSize = bufferSize;
     file->offset = 0;
     file->bufPosition = 0;
     file->userPointer = 0;
-    file->bufEnd = file->buf + bufferSize;
-    file->reading = 0;
+    file->lastOperationWrite = 0;
+    file->lastOperationRead = 0;
     
     return file;
 }
@@ -56,73 +61,81 @@ MYFILE *myopen(const char* pathname, int flags) {
  */
 
 // call syscall read
-void myread(MYFILE *file, void *buf, size_t nbyte){
-    ssize_t bytesRead;
+ssize_t myread(MYFILE *file, void *readBuf, size_t nbyte){
+    ssize_t bytesRead = 0;
+    off_t result;
+    if (file == NULL || readBuf == NULL) {
+        printf("%s\n", "file or readBuf is not declared/null value");
+        return -1;
+    }
     //check read permissions based on flags
-    if(file->flags!=O_RDONLY || file->flags!=O_RDWR){
-        return;
+    if (file->flags != O_RDONLY && file->flags != O_RDWR) {
+        printf("%s\n", "Not a valid read flag");
+        return -1;
+    }
+    if (nbyte == 0) {
+        return 0;
+    }
+    if (nbyte < 0) {
+        printf("%s\n", "Cannot read -ve number of bytes");
+        return -1;
+    }
+    if(file->lastOperationWrite == 1){
+        //call my flush
+        file->lastOperationWrite = 0;
     }
     // Check if the user is requesting more bytes than the struct buf size,
     // then there is no point of our buffer, call syscall read directly
     if (nbyte >= file->bufSize) {
-        bytesRead = read(file->fd, buf, nbyte);
-        if (bytesRead == -1) {
-            perror("read");
+        if(file->lastOperationRead) {
+            if((result = lseek(file->fd,file->userPointer,SEEK_SET)) == -1){
+                perror("lseek");
+                return -1;
+            }
+            file->userPointer = result;
         }
-        file->bufPosition = file->bufSize;
-        file->userPointer += bytesRead;
-        file->offset += bytesRead;
-        return;
-    }
-
-    if (!file->reading) {
-        bytesRead = read(file->fd, file->buf, file->bufSize);
-        if (bytesRead == -1) {
+        // maybe put this in a while loop, if nbyte > size of readBuf it will only give us size of readBuf bytes
+        if ((bytesRead = read(file->fd, readBuf, nbyte)) == -1) {
             perror("read");
+            return -1;
         }
-        file->bufPosition = file->bufSize;
-        file->userPointer = nbyte;
-        file->offset = 0;
-        file->reading = 1;
-    }
-    // want to ask for more than is passed(the user want) requires 
-    // will fail if the parameter nbyte exceeds INT_MAX, and they do not attempt a partial read.
-    // Check if there's any data available in the buffer (buf) starting from the current position (buf_pos).
-    // If there is data in the buffer, read from the buffer starting at the current position and update buffer position accordingly.
-    // If the buffer is empty or there isn't enough data to satisfy the read request, you may need to refill the buffer by reading from the file.
-    // call read and fill the buffer only when it is the first time using read or you just flushed read 
-    // also call read directly without buffer if bytes requested> buffer size
-    // Check if there's any data available in the buffer
-    if (file->bufPosition < 0 || file->bufPosition >= file->bufSize) {
-        // Buffer is empty or out of bounds, refill it
-        if ((bytesRead = read(file->fd, file->buf, file->bufSize)) == -1) {
-            perror("read");
-        }
+        // printf("bytes read directly %d\n", bytesRead);
+        //reset the buffer
         file->bufPosition = 0;
+        file->userPointer += bytesRead;
+    } else if(file->bufPosition <= file->bufSize){
+        //check if there is still room in buffer for the users request
+        if(file->bufPosition + nbyte >= file->bufSize){
+            //but the user has also requested more bytes which overflows the buf
+            //so call syscall read to fill the buffer again and continue with overflow
+            int firstCopySize = file->bufSize - file->bufPosition;
+            memcpy(readBuf, file->buf + file->bufPosition, firstCopySize);
+            if ((bytesRead = read(file->fd, file->buf, file->bufSize)) == -1) {
+                perror("read");
+                return -1;
+            }
+            //only copy the few overflowing bytes
+            memcpy(readBuf + firstCopySize, file->buf, (file->bufPosition + nbyte) - file->bufSize);
+            file->bufPosition = (file->bufPosition + nbyte) - file->bufSize;
+            file->userPointer += nbyte;
+        }else{
+            if(file->bufPosition == 0){
+                if ((bytesRead = read(file->fd, file->buf, file->bufSize)) == -1) {
+                    perror("read");
+                    return -1;
+                }
+            }
+            // Copy data into readBuf
+            memcpy(readBuf, file->buf + file->bufPosition, nbyte);
+            file->userPointer += nbyte;
+            file->bufPosition += nbyte;
+        }
     }
-    // Calculate how many bytes to read from the buffer
-    ssize_t bytesToRead = (nbyte < file->bufSize) ? nbyte : file->bufSize;
+    //if bufposition is zero fill the buffer
+    // will fail if the parameter nbyte exceeds INT_MAX, and they do not attempt a partial read.
 
-    if(file->userPointer>=file->bufPosition){
-        // flush
-    }
-    // // only requested amount should go into users buffer
-    // memcpy(buf,file->buf,nbyte);
-    // //Upon return from read(), the pointer is incremented by the number of bytes actually read.
-    // //Upon return from read(), the pointer in the struct buf is also increased to keep track of how much we have remaining
-    // file->userPointer=file->userPointer+bytesRead;
-    // file->bufPosition=file->bufPosition+bytesRead;
-    if (bytesToRead > 0) {
-        // Copy data from the buffer to the user's buffer
-        memcpy(buf, file->buf, bytesToRead);
-        
-        // Update pointers and offsets
-        file->userPointer += bytesToRead;
-        file->bufPosition += bytesToRead;
-        file->offset += bytesToRead;
-        bytesRead = bytesToRead;
-    }
-    // combine info based on if I have info on buffer instead of calling function again
+    file->lastOperationRead=1;
+    return bytesRead;
 }
 
 /*
@@ -130,17 +143,31 @@ void myread(MYFILE *file, void *buf, size_t nbyte){
  */
 
 // call syscall lseek, reposition pointers
-void myseek(MYFILE *file, off_t offset, int whence){
-    // Use lseek to set the new offset within the file
-    // Upon successful completion, lseek() returns the resulting offset location as measured in bytes from the beginning of the file.
-    // it returns a new position within the file, update struct offset to the return value
-    // useful to update this to continue reading at this position in file after you offset 
-    off_t result = lseek(file->fd, offset, whence);
-    if (result == -1) {
-        perror("lseek");
+ssize_t myseek(MYFILE *file, off_t offset, int whence){
+    off_t result;
+    int newOffset;
+    if (file == NULL) {
+        return -1;
     }
-    // Update offset in the fileInfo structure
+
+    if (whence == SEEK_SET) {
+        newOffset=file->userPointer+offset;
+    }else if (whence == SEEK_CUR) {
+        newOffset=offset;
+    }else{
+        printf("%s\n", "Not a valid whence value");
+        return -1;
+    }
+    //flush the buffer before seeking to prevent read and write data getting mixed by changing offset location and rereading/rewriting data
+    //flush()
+    if ((result = lseek(file->fd, newOffset, whence)) == -1) {
+        perror("lseek");
+        return -1;
+    }
+    file->userPointer=result;
     file->offset = result;
-    // Reset the buffer position
-    file->bufPosition = file->bufSize;
+    file->bufPosition = 0;
+    //update allpointer like user po
+    return result;
 }
+// in mywrite if last instruction was myread - then call lseek, offset should be beginning of file
--- test.c
+++ test.c
@@ -4,28 +4,56 @@
 #include <fcntl.h>
 #include "myio.h"
 
+void testRead(); 
+
 int main() {
+    MYFILE *file1;
+    MYFILE *file2;
+    MYFILE *file3;
+    ssize_t bytesRead;
+    int bytesRequested;
     // Test myopen
-    MYFILE *file;
-    if ((file = myopen("random.txt", O_RDONLY)) == NULL) {
+    if ((file1 = myopen("random.txt", O_RDONLY)) == NULL) {
         perror("myopen");
         return 1;
     }
 
+    // if ((file = myopen("sample.txt", O_WRONLY | O_TRUNC)) == NULL) {
+    //     perror("myopen");
+    //     return 1;
+    // }
+    
     // Test myread
-    char buffer[3];
-    size_t bytesToRead = sizeof(buffer);
-    myread(file, buffer, bytesToRead);
-    printf("Read from file: %s\n", buffer);
+    char buffer[8];
+    bytesRequested = 4;
+    bytesRead = myread(file1, buffer, bytesRequested);
+    printf("Read from file: %.*s", bytesRequested, buffer);
+    bytesRequested = 7;
+    bytesRead = myread(file1, buffer, bytesRequested);
+    printf("%.*s\n", bytesRequested, buffer);
 
-    // Test myseek
-    off_t newOffset = 10;
-    int whence = SEEK_SET; //  seek mode (e.g., SEEK_SET, SEEK_CUR, SEEK_END) from lseek
-    myseek(file, newOffset, whence);
-    printf("New file offset: %lld\n", (long long)file->offset);
+    if ((file2 = myopen("sample.txt", O_RDONLY)) == NULL) {
+        perror("myopen");
+        return 1;
+    }
 
-    // Clean up and close the file
-    // ...
+    bytesRead = myread(file2, buffer, bytesRequested);
+    printf("%.*s", bytesRequested, buffer);
+    bytesRequested = 8;
+    bytesRead = myread(file2, buffer, bytesRequested);
+    printf("%.*s\n", bytesRequested, buffer);
+    bytesRequested = 15;
+    bytesRead = myread(file1, buffer, bytesRequested);
+    printf("%.*s\n", bytesRequested, buffer);
+
+    if ((file3 = myopen("sample2.txt", O_WRONLY | O_TRUNC)) == NULL) {
+        perror("myopen");
+        return 1;
+    }
 
     return 0;
+}
+
+void testRead() {
+    ; 
 }
