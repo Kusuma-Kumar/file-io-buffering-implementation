a7d0ec0dd3b69a0b2752eddb8fade1b71bb6f140
Kusuma Kumar <kusumak@middlebury.edu>
2023-11-29 13:56:27 -0500

    Revert "Revert "i commented out most of the tests to make it easier to see where segfault happens""
    This reverts commit 1facf6215128f3e1d4a1b688bee8c4dd05ff3db7.

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 #include "myio.h"
 
 int main() {
+    /*
     MYFILE *file1;
     MYFILE *file2;
     MYFILE *file3;
@@ -16,10 +17,10 @@ int main() {
     if((file1 = myopen("input files/file1.txt", O_RDONLY)) == NULL) {
         return -1;
     }
-
+    */
     // Test myread
     char readBuf[16];
-
+/*
     // any request to read of nbyte > sizeof(readBuf) will only give you sizeof(readBuf) bytes as that the max the readBuf can handle
     // test for initial read
     bytesRequested = 16;
@@ -158,9 +159,11 @@ int main() {
     if((file1 = myopen("input files/file4.txt", O_CREAT | O_WRONLY )) == NULL) {
         return -1;
     }
-
+*/
     // Test mywrite 
     char data4[] = "blahhhh";
+
+    /*
 
     if(mywrite(file1, data4, strlen(data4)) == -1) {
         return -1;
@@ -189,6 +192,7 @@ int main() {
     if(myclose(file5) == -1) {
         return -1;
     }
+    */
 
     MYFILE *fileMix;
     if((fileMix = myopen("input files/mixReadandWrite.txt", O_RDWR)) == NULL) {
@@ -199,8 +203,10 @@ int main() {
     myread(fileMix, readBuf, 8);
     mywrite(fileMix, data4, strlen(data4));
     myread(fileMix, readBuf, 30);
+    //Its after this read that whatever is called next (read or write) we get a segfault 
     mywrite(fileMix, data4, strlen(data4));
     myread(fileMix, readBuf, 8);
+    mywrite(fileMix, data4, strlen(data4));
 
     return 0;
 }
